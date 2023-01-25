c7d8884e2d96396c94fddbfa24be0e5b728439f6
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2023-01-26 02:12:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const chartData = {
+  labels: ["Python", "Java", "JavaScript", "C#", "Others"],
+  data: [30, 17, 10, 7, 36],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: "Language Popularity",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 10,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
